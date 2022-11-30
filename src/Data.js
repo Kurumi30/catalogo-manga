9473e343393ab0f75aec9f6dde1f8fac1951ee11
Kurumi30 <bsd3su@gmail.com>
2022-11-30 00:29:06 -0300

    first commit

---
+++ src/Data.js
@@ -0,0 +1,98 @@
+let shopItemsData = [
+  {
+    id: "spyxfamily",
+    name: "Spy X Family",
+    author: "Tatsuya Endou",
+    desc: 'O mestre espião de codinome "Twilight" passou seus dias em missões secretas, tudo...',
+    img: 'https://static3.mangalivre.net/capas/d4BGQz7-BNNxMyAf1G4PCA/13348/6112891ddd175external_cover.jpg',
+    link: "spy-x-family-colorida/13348",
+  },
+  {
+    id: "bungoustraydogs",
+    name: "Bungou Stray Dogs",
+    author: "Sango Harukawa",
+    desc: "Nakajima Atsushi foi expulso de seu orfanato, e agora ele não tem nenhum lugar par...",
+    img: "https://static3.mangalivre.net/capas/WivQlzCHw8G5Ob9uTIO__g/3373/620b3743d3b37external_cover.jpg",
+    link: "bungou-stray-dogs/3373",
+  },
+  {
+    id: "jujutsukaisen",
+    name: "Jujutsu Kaisen",
+    author: "Gege Akutami",
+    desc: "Yuji é um gênio do atletismo, mas não tem interesse algum em ficar correndo em círculos...",
+    img: "https://static3.mangalivre.net/capas/iGWDKWLc8XQ3JYrSQ18fBA/7178/634dbac5b95b0_external_cover.jpg",
+    link: "jujutsu-kaisen/7178",
+  },
+  {
+    id: "overlord",
+    name: "Overlord",
+    author: "Kugane Maruyama",
+    desc: 'A história começa com Yggdrasil, um popular jogo online que está tranquilamente encerrando...',
+    img: "https://static3.mangalivre.net/capas/n2-TgTqNhO4nV9JlC5BSrA/1936/62c62c4aa1b81_external_cover.jpg",
+    link: "overlord/1936",
+  },
+  {
+    id: "myheroacademia",
+    name: "Boku no Hero Academia",
+    author: "Kohei Horikoshi",
+    desc: "A série conta a história de Midoriya Izuku, um garoto tímido e fraco cujo sonho é ser um herói...",
+    img: "https://static3.mangalivre.net/capas/99Pf16uyRuIkZopMx1q9Xw/1319/632dd2be8fb76_external_cover.jpg",
+    link: "boku-no-hero-academia/1319",
+  },
+  {
+    id: "demonslayer",
+    name: "Kimetsu no Yaiba",
+    author: "Gotouge, Koyoharu",
+    desc: "O mangá conta a história de Tanjiro, o filho mais velho de uma família que é massacrada por um...",
+    img: "https://static3.mangalivre.net/capas/lRAq3QFSQQWlYp4-XojAOw/3364/6037ed57c7fd3external_cover.jpg",
+    link: "kimetsu-no-yaiba/3364",
+  },
+  {
+    id: "assassinationclassroom",
+    name: "Ansatsu Kyoushitsu",
+    author: "Yusei Matsui",
+    desc: "A Classe 3-E da escola Kunugigaoka é apresentada a uma figura bem estranha. Acontece que ele é um monstro...",
+    img: "https://static3.mangalivre.net/capas/AvhpkIHTLYVX9TVjo8fwhA/862/external_cover.jpg",
+    link: "ansatsu-kyoushitsu/862",
+  },
+  {
+    id: "misskobayashisdragonmaid",
+    name: "Kobayashi-san Chi no Maid Dragon",
+    author: "Cool-kyou Shinja",
+    desc: "Kobayashi vive sozinha em um apartamento, até que um dia, Tooru, a garota dragão, apareceu e elas acabaram moran...",
+    img: "https://static3.mangalivre.net/capas/mTyBPjCgXsTcPpg5NOd3SQ/3424/620b2e75dc68aexternal_cover.jpg",
+    link: "kobayashi-san-chi-no-maid-dragon/3424",
+  },
+  {
+    id: "chainsawman",
+    name: "Chainsaw Man",
+    author: "Tatsuki Fujimoto",
+    desc: "Quando seu pai morreu, Denji ficou preso com uma dívida que era incapaz de pagar. Mas graças à ajuda de u...",
+    img: "https://static3.mangalivre.net/capas/y0k4l0OnPIoKgRe7pMUeCQ/12288/605b34f9dff68external_cover.jpg",
+    link: "chainsaw-man-edicao-colorida/12288"
+  },
+  {
+    id: "tonikakukawaii",
+    name: "Tonikaku Kawaii",
+    author: "Kenjiro Hata",
+    desc: "Regredindo 1400 anos no tempo, de volta ao começo da eternidade-- Quanta dor a mais... E quantos incontáveis anos...",
+    img: "https://static3.mangalivre.net/capas/71J7kXAMD3Tqv_Mthh8Sgw/7095/external_cover.jpg",
+    link: "tonikaku-kawaii/7095"
+  },
+  {
+    id: "thepromissedneverland",
+    name: "Yakusoku no Neverland",
+    author: "Kaiu Shirai",
+    desc: "Emma e seus amigos vivem uma boa vida no orfanato em que cresceram. embora as regras sejam duras, a babá...",
+    img: "https://static3.mangalivre.net/capas/XxG_-E0-93XIygjOX_lGqQ/4055/external_cover.jpg",
+    link: "yakusoku-no-neverland/4055"
+  },
+  {
+    id: "classroomoftheelite",
+    name: "Youkoso Jitsuryoku",
+    author: "Shougo Kinugasa",
+    desc: " colégio Koudo Ikusei, é uma colégio com instalações de última geração, onde quase 100% dos seus alunos conseguem en...",
+    img: "https://static3.mangalivre.net/capas/rI0zs3GV-fWeX64JxQUB2w/6836/62507ab6dc2ad_external_cover.jpg",
+    link: "youkoso-jitsuryoku-shijou-shugi-no-kyoushitsu-e/6836"
+  },
+];
